3670b3209dc83fa31c804be3e0338a6d56ae402c
Maksi <maksi.mamaeva2003@gmail.com>
2023-05-14 00:10:16 +0300

    first commit

---
+++ обучениепрактикаунивер/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace обучениепрактикаунивер
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.aTextBox = new System.Windows.Forms.TextBox();
+            this.bTextBox = new System.Windows.Forms.TextBox();
+            this.h1TextBox = new System.Windows.Forms.TextBox();
+            this.cTextBox = new System.Windows.Forms.TextBox();
+            this.dTextBox = new System.Windows.Forms.TextBox();
+            this.h2TextBox = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.TextBoxMin = new System.Windows.Forms.TextBox();
+            this.Table = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // aTextBox
+            // 
+            this.aTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.aTextBox.Location = new System.Drawing.Point(945, 51);
+            this.aTextBox.Name = "aTextBox";
+            this.aTextBox.Size = new System.Drawing.Size(100, 26);
+            this.aTextBox.TabIndex = 0;
+            // 
+            // bTextBox
+            // 
+            this.bTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.bTextBox.Location = new System.Drawing.Point(945, 112);
+            this.bTextBox.Name = "bTextBox";
+            this.bTextBox.Size = new System.Drawing.Size(100, 26);
+            this.bTextBox.TabIndex = 1;
+            // 
+            // h1TextBox
+            // 
+            this.h1TextBox.Location = new System.Drawing.Point(945, 173);
+            this.h1TextBox.Name = "h1TextBox";
+            this.h1TextBox.Size = new System.Drawing.Size(100, 22);
+            this.h1TextBox.TabIndex = 2;
+            // 
+            // cTextBox
+            // 
+            this.cTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.cTextBox.Location = new System.Drawing.Point(945, 234);
+            this.cTextBox.Name = "cTextBox";
+            this.cTextBox.Size = new System.Drawing.Size(100, 26);
+            this.cTextBox.TabIndex = 3;
+            // 
+            // dTextBox
+            // 
+            this.dTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.dTextBox.Location = new System.Drawing.Point(945, 295);
+            this.dTextBox.Name = "dTextBox";
+            this.dTextBox.Size = new System.Drawing.Size(100, 26);
+            this.dTextBox.TabIndex = 4;
+            // 
+            // h2TextBox
+            // 
+            this.h2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.h2TextBox.Location = new System.Drawing.Point(945, 356);
+            this.h2TextBox.Name = "h2TextBox";
+            this.h2TextBox.Size = new System.Drawing.Size(100, 26);
+            this.h2TextBox.TabIndex = 5;
+            // 
+            // button
+            // 
+            this.button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button.Location = new System.Drawing.Point(892, 472);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(195, 44);
+            this.button.TabIndex = 6;
+            this.button.Text = "построить таблицу";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // TextBoxMin
+            // 
+            this.TextBoxMin.Location = new System.Drawing.Point(945, 411);
+            this.TextBoxMin.Multiline = true;
+            this.TextBoxMin.Name = "TextBoxMin";
+            this.TextBoxMin.Size = new System.Drawing.Size(100, 28);
+            this.TextBoxMin.TabIndex = 7;
+            // 
+            // Table
+            // 
+            this.Table.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Table.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Table.Location = new System.Drawing.Point(27, 51);
+            this.Table.Name = "Table";
+            this.Table.RowTemplate.Height = 24;
+            this.Table.Size = new System.Drawing.Size(805, 465);
+            this.Table.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(894, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "a =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(894, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "b =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(894, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "h1 =";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(894, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "c =";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(894, 295);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "d =";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.Location = new System.Drawing.Point(894, 356);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "h2 =";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(888, 419);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Min =";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label8.Location = new System.Drawing.Point(23, 547);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(180, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Время выполнения: ";
+            this.label8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(892, 535);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 44);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Выход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(1105, 596);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Table);
+            this.Controls.Add(this.TextBoxMin);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.h2TextBox);
+            this.Controls.Add(this.dTextBox);
+            this.Controls.Add(this.cTextBox);
+            this.Controls.Add(this.h1TextBox);
+            this.Controls.Add(this.bTextBox);
+            this.Controls.Add(this.aTextBox);
+            this.Name = "Form1";
+            this.Text = "Практическое задание";
+            ((System.ComponentModel.ISupportInitialize)(this.Table)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox aTextBox;
+        private System.Windows.Forms.TextBox bTextBox;
+        private System.Windows.Forms.TextBox h1TextBox;
+        private System.Windows.Forms.TextBox cTextBox;
+        private System.Windows.Forms.TextBox dTextBox;
+        private System.Windows.Forms.TextBox h2TextBox;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.TextBox TextBoxMin;
+        private System.Windows.Forms.DataGridView Table;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ обучениепрактикаунивер/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace обучениепрактикаунивер
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            double a, b, c, d, h1, h2;
+            a = Convert.ToDouble(aTextBox.Text);
+            b = Convert.ToDouble(bTextBox.Text);
+            h1 = Convert.ToDouble(h1TextBox.Text);
+            c = Convert.ToDouble(cTextBox.Text);
+            d = Convert.ToDouble(dTextBox.Text);
+            h2 = Convert.ToDouble(h2TextBox.Text);
+            Solution(a, b, h1, c, d, h2);
+
+        }
+        private double Function(double x, double y)
+        {
+            return (x * Math.Pow((x - 1) + y, 1.0 / 3.0));
+        }
+        private void Solution(double a, double b, double h1, double c, double d, double h2)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            int n1, n2;
+            double x, y, min, func;
+            n1 = GetCount(a, b, h1);
+            n2 = GetCount(c, d, h2); 
+            CreateTable(n1, n2);
+            x = a;
+            y = c;
+            min = Double.MaxValue;
+            for (int i = 0; i < n1; i++)
+            {
+                Table.Columns[i].Name = Convert.ToString(Math.Round(a+h1*i, 6));
+            }
+            for (int j = 0; j < n2; j++)
+            {
+                Table.Rows[j].HeaderCell.Value = Convert.ToString(Math.Round(c + h2 * j, 6));
+                
+            }
+            for (int j = 0; j < n2; j++)
+            {
+                x = a;
+                for (int i = 0; i < n1; i++)
+                {
+                    func = Function(x, y);
+                    Table.Rows[j].Cells[i].Value = Math.Round(func, 6);
+                    x += h1;
+                    if (func < min)
+                        min = func;
+                }
+                y += h2;
+            }
+            OutputMin(min, TextBoxMin);
+            watch.Stop();
+            label8.Text = $"Время выполнения: {watch.ElapsedMilliseconds.ToString()}  ms.( {n1.ToString()} x {n2.ToString()} = {(n1 * n2).ToString()} ячеек)"; 
+
+        }
+        private int GetCount (double start, double finish, double step)
+        {
+            int n = Convert.ToInt32(Math.Floor(Math.Round((finish - start) / step + 1, 6)));
+            return n;
+        }
+        private void OutputMin(double min, TextBox textBoxMin)
+        {
+            textBoxMin.Text = Convert.ToString(Math.Round(min, 6));
+        }
+        private void CreateTable(int n1, int n2)
+        {
+            Table.ColumnCount = n1;
+            Table.RowCount = n2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
